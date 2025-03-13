84e56eb9083f7c4dc9f09b39b74391d9e72cd475
neiranie <nurkhairanianneira@gmail.com>
2025-03-13 11:24:09 +0700

    first commit

---
+++ Bubble_Sort2.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int arr[20];    // Deklarasi variable global array dengan panjang 20
+int n;  // Deklarasi variable global n untuk menyimpan elemen pada array
+
+void input () {
+    int d;
+    while (true) {
+        cout << "Masukkan banyak elemen pada Array:";
+        cin >> n;
+        if (n <= 20){
+            break;
+        }
+        else {
+            cout << "\nArray maksimal 20 elemen.\n";
+        }
+    }
+    cout << endl;
+    cout << "====================" << endl;
+    cout << "Masukkan Elemen Array" << endl;
+    cout << "====================" << endl;
+
+    for (int i=0;i<n;i++){
+        cout << "Data ke-" << (i+1) << "=";
+        cin >> arr[i];
+    }
+}
+
+void bubbleSortArray(){
+    int pass = 1;   // step 1
+    do {
+        for (int j=0;j<=n-1-pass;j++){  // step 2
+            if (arr[j] > arr[j+1]) {        // step 3
+                int temp;
+                temp = arr[j];
+                arr[j] = arr [j+1];
+                arr[j+1] = temp;
+            }
+        }
+        pass = pass + 1; // step 4 
+    }while(pass <= n-1); // step 5
+}
+
+void display(){
+    cout << endl;
+    cout << "====================" << endl;
+    cout << "Elemen Array yang telah tersusun" << endl;
+    cout << "====================" << endl;
+    for (int j=0;j<n;j++){
+        cout << arr[j];
+        if (j<n-1) {
+            cout << " --> ";
+        }
+    }
+}
+
+int main(){
+    input();
+    bubbleSortArray();
+    display();
+}
